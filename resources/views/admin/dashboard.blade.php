d6746273654e338c550d95a71487feb2c2c5ad7c
Pablo Fernando Navarro <pablofernandonavarro@gmail.com>
2020-07-30 07:40:12 -0300

    admin lte

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout')
+
+@section('content')
+<h1>hola mundo</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
+use Facade\FlareClient\View;
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Return_;
 
 
+//  ROUTE ADMIN
+
+Route::get('admin', function(){
+    return View('admin.dashboard');
+});
+
+
+
+//  /ROUTE ADMIN
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('products', 'productController');
+
