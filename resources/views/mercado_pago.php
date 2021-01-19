0dcc998a13e3a188659ac72eaadfcb1734c2d47d
pablofernandonavarro <pablofernandonavarro@gmail.com>
2021-01-19 11:54:47 -0300

    start MP

---
+++ resources/views/mercado_pago.php
@@ -0,0 +1,27 @@
+<?php
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
